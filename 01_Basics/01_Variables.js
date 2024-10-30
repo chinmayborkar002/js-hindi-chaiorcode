8e20511eb9dac7642608de5249e345394b99e856
chinmayborkar002 <chinmayborkar002@gmail.com>
2024-10-30 13:06:49 +0000

    done

---
+++ 01_Basics/01_Variables.js
@@ -0,0 +1,16 @@
+const accountId = 986056;
+let accountEmail = "chinmay@gmail.com";
+var accountPassword = "95647";
+accountCity = "Jaipur";
+let accountState
+
+//  accountId = 786565; // not allowed
+
+
+accountEmail = "aniket@gmail.com";
+accountPassword = "095645";
+accountCity = "Nagpur";
+
+console.log(accountId);
+
+console.table([accountEmail, accountPassword, accountCity, accountId, accountState]);
