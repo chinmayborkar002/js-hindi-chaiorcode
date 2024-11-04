0fae15cb369edcb440fdc231f30c6009515073cb
chinmayborkar002 <chinmayborkar002@gmail.com>
2024-11-04 16:47:45 +0000

    Conversion of Datatypes

---
+++ 01_Basics/03_ConversionOperation.js
@@ -0,0 +1,33 @@
+let score = "33ans"
+
+// console.log(typeof score);
+// console.log(typeof (score));
+
+let valueInNumber = Number(score);
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+// "33" => 33
+// "33qbc"=> NaN (Not a Number) but the type of NaN is Number Actually.....
+// true =>1
+//false =>0
+
+// we done number conversion but boolean conversion is there also...
+
+let isLoggedIn = "chinmay"
+
+let booleanLoggedIn = Boolean(isLoggedIn);
+// console.log(typeof booleanLoggedIn);
+// console.log(booleanLoggedIn);
+
+// 1 => true 
+// 0 => false
+// "" => false
+// "chinmay" => true
+
+let Num = 45;
+
+let stringNum = String(Num);
+console.log(typeof Num);
+console.log(typeof stringNum);
+console.log(stringNum);
